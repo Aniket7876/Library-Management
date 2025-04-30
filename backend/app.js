24536996d8a71239927f3f101d4e0e0a7513dffb
Aniket7876 <aniketgore700@gmail.com>
2025-04-30 12:17:56 +0530

    origin

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ app.use(morgan(':method :url :status'));
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
